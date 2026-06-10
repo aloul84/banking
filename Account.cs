221a9806b05e2d2fb45243f1f7a5544396134108
Rami Salameh <rami.mms2@gmail.com>
2026-06-10 13:51:47 +0300

    create functions

--- Account.cs
+++ Account.cs
@@ -1,6 +1,30 @@
-namespace ;
 
-public class account
+
+
+public class Account
 {
+int id;
+string name;
+string type;
+
+public string GetName()
+
+    {
+       return name;
+    }
+
+    public int GetId()
+    {
+        return id;
+    }
+    public string Gettype()
+    {
+        return type;
+    }
+    public void SetName( string FullName)
+    {
+        name = FullName;
+    }
+
 
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Account account1 = new Account() ;
+account1.SetName("Rami");
+account1.GetName();
+Console.WriteLine(account1.GetName());
+
+
