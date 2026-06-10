3146de50051e599dc9cdd57eb43e298e61da8623
MoathAbuHafeetha <moath.abuhafeetha@gmail.com>
2026-06-10 13:34:23 +0300

    ModifyLogin_Program

--- Logins.cs
+++ Logins.cs
@@ -1,1 +1,11 @@
-class Logins{}
+class Logins
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Logins(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter ID: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Logins user = new Logins(id, name);
+
+        Console.WriteLine("User Created:");
+        Console.WriteLine(name);
+    }
+}
