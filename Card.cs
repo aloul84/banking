c18ec6919dc80a8c05c8200c52eb00a3ec257b2b
Ali <spiritbird86@gmail.com>
2026-06-14 09:22:31 +0300

    edit cards

---
+++ Card.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+
+public class Card
+{
+
+  private int cardId;
+  private string cardName;
+
+
+ public  Card(string cName)
+    {
+        Random rand = new Random();
+        cardId = rand.Next(1, 100);
+        cardName=cName;
+    }
+  
+    public  void printCard()
+
+    {
+
+      Console.WriteLine("User name is " + cardName);
+      Console.WriteLine("card id is " + cardId);
+
+     
+      }
+
+
+
+
+
+}
