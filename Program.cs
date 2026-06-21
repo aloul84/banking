427034646ab3b86ece2ea42b5c3f17ea3d36f7f3
Rami Salameh <rami.mms2@gmail.com>
2026-06-21 13:26:47 +0300

    finish account

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
-﻿
-
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter ID: ");
+        
+     //----------------account create-----------------------------//
+
+       Console.WriteLine("Enter Accont id:");
+       int ID = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Account Name:");
+       string NAME=Console.ReadLine();
+       //Console.WriteLine("EnterAccount Type:");
+      // string TYPE = Console.ReadLine();
+       string [] AccountType = new string [3] {"ILS","USD","JOD"};
+       string TYPE;
+            Console.WriteLine("EnterAccount Type:");
+                TYPE = Console.ReadLine();
+             
+             while(! AccountType.Contains(TYPE))
+        {
+                          Console.WriteLine("Input Valid Value: ILS or USD or JOD");
+                           Console.WriteLine("Enter Account Type:");
+                TYPE = Console.ReadLine();
+
+        }
+       Console.WriteLine("Account Created");
+       Console.WriteLine($"Account Informations Is: "+"id:"+ID +"  Name:"+NAME + "  Type:"+TYPE);
+ 
+
+  //--------------------------Account Created--------------------------------------//
+  Console.Write("Enter ID: ");
         int id = int.Parse(Console.ReadLine());
 
         Console.Write("Enter Name: ");
@@ -17,22 +40,5 @@ class Program
         Console.WriteLine("User Created:"+name);
         Console.WriteLine("User Created:");
         Console.WriteLine(name);
-       //account create
-       Console.WriteLine("Enter Accont id:");
-       int ID = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter Account Name:");
-       string NAME=Console.ReadLine();
-       Console.WriteLine("EnterAccount Type:");
-       string TYPE = Console.ReadLine();
-       Console.WriteLine("Account Created");
-
-
-
-      Console.WriteLine("Hello, World!");
-
-      Card cardemp1 = new Card("sami");
-
-      cardemp1.printCard();
- 
     }
 }
