0c1593edd73aa4cd1ca51133479eee1398b1317f
amalfarra <aseelamal93@gmail.com>
2026-06-21 13:29:28 +0300

    Update loans code

--- loans.cs
+++ loans.cs
@@ -3,24 +3,30 @@ public class loans
 {
 public int id;
 public double  amunt;
-
+public int months;      
 public loans(int idloans, double amuntadd    )
     {
         id = idloans;
 amunt=amuntadd;
     }
-public int getId()
+
+    public int getId()
     {
         return id;
 
 
+    }
+    public double Months()
+    {
+
+        return months;
     }
 
 public double getamunt()
     {
 
         return amunt;
-    } 
+    }
 
 
 
